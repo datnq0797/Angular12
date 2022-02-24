a0f2cd96a8f0b257f9cf365d3ba29021bc7f00e3
DESKTOP-4CE1EK8\Admin <ndat3931@gmail.com>
2022-02-24 13:42:23 +0700

    twowaybi

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-home',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public name = 'dat'
+  public age = 20
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  public reset(){
+    this.name=""
+    this.age=0
+    console.log('resetname')
+  }
 }
